979dcfccb52d93467fdee3e9e22ba18145ac4569
DESKTOP-1SDFEC4\keisuke(ByDELL) <keisuke.wakasa.s11t286@gmail.com>
2023-07-15 17:33:50 +0900

    signページ編集

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,75 @@ function addRow(){
 function deleteRow(rowno){
   $("#"+rowno).remove();
 }
+
+//サーバと疎通して、ログインする
+function loginToServer(){
+  //TODO:本番用と切り替える
+  //const API_URL = "https://bejewelled-arithmetic-214844.netlify.app/.netlify/functions/SlackNotice";
+  const API_URL = "http://localhost:9000/.netlify/functions/login";
+
+
+  //入力内容を取得
+ var email = document.getElementById("email").value;
+ var pw = document.getElementById("pw").value;
+
+ //コメントをスラックに送信する
+ let body = {
+   email: msg,
+   password:pw
+ };
+
+ // バックエンドAPIへPOST
+ const payload = JSON.stringify(body);
+
+ fetch(API_URL, {
+   method: "POST",
+   body: payload,
+ })
+
+   .then((response) => {
+     alert("Slackへ送信しました")
+   })
+   
+   .catch(error => {
+     console.log(error);
+     alert("通信エラーが発生しました。再度送信ください。");
+   });
+}
+
+//サーバと疎通して、ユーザを新規登録する
+function createAccout(){
+  //TODO:本番用と切り替える
+  //const API_URL = "https://bejewelled-arithmetic-214844.netlify.app/.netlify/functions/SlackNotice";
+  const API_URL = "http://localhost:9000/.netlify/functions/AccountCreate";
+
+
+  //入力内容を取得
+ var email = document.getElementById("email").value;
+ var pw = document.getElementById("pw").value;
+ var name = document.getElementById("name").value;
+
+ //コメントをスラックに送信する
+ let body = {
+   email: email,
+   password:pw,
+   name:name
+ };
+
+ // バックエンドAPIへPOST
+ const payload = JSON.stringify(body);
+
+ fetch(API_URL, {
+   method: "POST",
+   body: payload,
+ })
+
+   .then((response) => {
+     alert("OK")
+   })
+   
+   .catch(error => {
+     console.log(error);
+     alert("通信エラーが発生しました。再度送信ください。");
+   });
+}
