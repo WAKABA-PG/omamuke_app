3c715947a7a289b41bb5939835e455dcab486c79
DESKTOP-1SDFEC4\keisuke(ByDELL) <keisuke.wakasa.s11t286@gmail.com>
2023-08-17 13:37:17 +0900

    修正

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 function checkStart() {
   $("#checkInput").css('display', 'block');
+
+  getListData(getParamId());
 }
 
 //リスト編集のインデックス番号
@@ -46,7 +48,7 @@ function loginToServer() {
       var re = data.result;
       if (re === "OK") {
 
-        location.href = "../index.html?name=" + data.name + "&email=" + data.email
+        location.href = "../index.html?name=" + data.name + "&email=" + data.email + "&id=" + data.id
       } else {
         alert("ログインできませんでした。メールアドレス、パスワードを確認してください。")
       }
@@ -93,7 +95,7 @@ function createAccout() {
       console.log(data.result);
       var re = data.result;
       if (re === "OK") {
-        location.href = "../index.html?name=" + data.name + "&email=" + data.email
+        location.href = "../index.html?name=" + data.name + "&email=" + data.email + "&id=" + data.id
       } else if (re === "NG1") {
         alert("すでに使用されているメールアドレスです。別のメールアドレスで登録してください。")
       } else {
@@ -113,6 +115,51 @@ function setDisplayName(target) {
   // URLSearchParamsオブジェクトを取得
   let params = url.searchParams;
   $("#" + target).append(params.get('name'))
-
-
 }
+
+function getParamId() {
+  // URLを取得
+  let url = new URL(window.location.href);
+  // URLSearchParamsオブジェクトを取得
+  let params = url.searchParams;
+  return params.get('id')
+}
+
+//項目リストをDBから取得する
+function getListData(id){
+  //TODO:本番用と切り替える
+  //const API_URL = "https://bejewelled-arithmetic-214844.netlify.app/.netlify/functions/SlackNotice";
+  const API_URL = "http://localhost:9000/.netlify/functions/GetList?id="+id;
+
+
+  fetch(API_URL, {
+    method: "GET",
+  })
+
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data.items);
+      var items = data.items;
+      for(var i=0; i<items.length; i++){
+        var td1 = "<tr><td>"+String(i+1)+"</td>"
+        var td2 = "<td>"+items[i]+"</td>"
+        var td3 = "<td style='text-align: center;'><input type='checkbox'></td></tr>"
+        
+        $("#list_table").append(td1 + td2 + td3)
+
+      }
+
+      
+      
+
+    })
+    .catch(error => {
+      console.log(error);
+      alert("通信エラーが発生しました。再度送信ください。");
+    });
+}
+
+
+
